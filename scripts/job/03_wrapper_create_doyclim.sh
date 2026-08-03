b34844b1a4ccaaf21b5924570a28122ebcfaa374
fadhlilmuhammad <fm6730@gadi-cpu-bdw-0006.gadi.nci.org.au>
2026-08-03 16:23:41 +1000

    update

--- scripts/job/03_wrapper_create_doyclim.sh
+++ scripts/job/03_wrapper_create_doyclim.sh
@@ -1,4 +1,5 @@
-for var in u v w q t; do 
+# for var in u v w q t; do 
+for var in q1; do 
     qsub -v var="$var" \
         /home/563/fm6730/localrepo/wave_vertical_heating/scripts/job/03_create_doyclim.qsub 
 done
--- scripts/main/create_doyanom.py
+++ scripts/main/create_doyanom.py
@@ -9,17 +9,24 @@ from pathlib import Path
 
 # %%
 var=sys.argv[1]
-folder = Path(f"/home/563/fm6730/localrepo/wave_vertical_heating/data/temp/wave_vertical_heating/{var}/era5/")
-folder_out = Path(f"/home/563/fm6730/localrepo/wave_vertical_heating/data/derived/daily_clim/era5/{var}/")
+varfolder=sys.argv[2]
+ens=sys.argv[3]
+# year=sys.argv[3]
+
+folder = Path(f"/home/563/fm6730/localrepo/wave_vertical_heating/data/derived/daily_clim/era5/{varfolder}/")
+folder_out = Path(f"/home/563/fm6730/localrepo/wave_vertical_heating/data/derived/daily_anom/access_s2/{varfolder}/")
+folder_in = Path(f"/home/563/fm6730/localrepo/wave_vertical_heating/data/derived/q1_vert_structure/{ens}/")
+
+os.makedirs(folder_out, exist_ok=True)
 
 # %%
-ds_clim = xr.open_mfdataset(os.path.join(folder,f"{var}_era5_oper_pl_merge*.nc"), chunks="auto")
+ds_clim = xr.open_dataset(os.path.join(folder,f"{varfolder}_era5_doyclim_1979-2020.nc"), chunks="auto")
+ds_data = xr.open_dataset()
 
 # %%
-daily_clim = ds_clim.groupby('time.dayofyear').mean('time')
+da_clim = ds_clim[var]
 
-# %%
-ds_clim
+
 
 # %%
 daily_clim.to_netcdf(os.path.join(folder_out,f"{var}_era5_doyclim_1979-2020.nc"))
